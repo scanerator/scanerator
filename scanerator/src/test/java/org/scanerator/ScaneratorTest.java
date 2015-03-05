efef27ec9f7606fb7fbd5095e45ace8feb2d9c93
robin <robin.kirkman@gmail.com>
2015-03-04 23:57:24 -0500

    Expand testAll

--- scanerator/src/test/java/org/scanerator/ScaneratorTest.java
+++ scanerator/src/test/java/org/scanerator/ScaneratorTest.java
@@ -9,8 +9,9 @@ import static org.scanerator.Scanerator.*;
 
 @SuppressWarnings("unchecked")
 public class ScaneratorTest {
-	private static final OrderedIterable<Integer> mul2 = itr(Arrays.asList(2, 4, 6, 8, 10, 12));
-	private static final OrderedIterable<Integer> mul3 = itr(Arrays.asList(3, 6, 9, 12));
+	private static final OrderedIterable<Integer> mul2 = itr(Arrays.asList(2, 4, 6, 8, 10, 12, 14));
+	private static final OrderedIterable<Integer> mul3 = itr(Arrays.asList(3, 6, 9, 12, 15));
+	private static final OrderedIterable<Integer> mul4 = itr(Arrays.asList(4, 8, 12));
 
 	@Test
 	public void testItr() {
@@ -27,16 +28,18 @@ public class ScaneratorTest {
 		Assert.assertEquals(Arrays.asList(6, 12), list(all(Arrays.asList(mul2, mul3))));
 		
 		Assert.assertEquals(list(mul2), list(all(Arrays.asList(mul2, mul2, mul2, mul2, mul2))));
+		
+		Assert.assertEquals(Arrays.asList(12), list(all(Arrays.asList(mul2, mul3, mul4))));
 	}
 	
 	@Test
 	public void testAny() {
-		Assert.assertEquals(Arrays.asList(2,3,4,6,6,8,9,10,12,12), list(any(Arrays.asList(mul2, mul3))));
+		Assert.assertEquals(Arrays.asList(2,3,4,6,6,8,9,10,12,12,14, 15), list(any(Arrays.asList(mul2, mul3))));
 	}
 	
 	@Test
 	public void testNot() {
-		Assert.assertEquals(Arrays.asList(2, 4, 8, 10), list(mul2.not(mul3)));
+		Assert.assertEquals(Arrays.asList(2, 4, 8, 10, 14), list(mul2.not(mul3)));
 	}
 	
 	@Test
