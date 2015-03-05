3a4e11a091466359397e6a261f6c86cbbd11dd12
robin <robin.kirkman@gmail.com>
2015-03-04 20:48:43 -0500

    Tests for WrappingOrderedIterable

---
+++ scanerator/src/test/java/org/scanerator/WrappingOrderedIterableTest.java
@@ -0,0 +1,33 @@
+package org.scanerator;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.scanerator.Scanerator.*;
+
+public class WrappingOrderedIterableTest {
+	@Test
+	public void testDropDescending() {
+		OrderedIterable<Integer> mul2 = new WrappingOrderedIterable<Integer>(
+				Arrays.asList(2, 4, 0, 6, 8),
+				Comparators.naturalOrder(),
+				true);
+		Assert.assertEquals(Arrays.asList(2, 4, 6, 8), list(mul2));
+	}
+	
+	@Test
+	public void testFailDescending() {
+		OrderedIterable<Integer> mul2 = new WrappingOrderedIterable<Integer>(
+				Arrays.asList(2, 4, 0, 6, 8),
+				Comparators.naturalOrder(),
+				false);
+		try {
+			list(mul2);
+			Assert.fail();
+		} catch(IllegalStateException e) {
+			// expected
+		}
+	}
+}
