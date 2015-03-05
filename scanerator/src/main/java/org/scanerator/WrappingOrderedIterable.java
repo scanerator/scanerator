a8307ad3457fccf5566a20807684370d178a3246
robin <robin.kirkman@gmail.com>
2015-03-04 23:30:59 -0500

    javadoc

--- scanerator/src/main/java/org/scanerator/WrappingOrderedIterable.java
+++ scanerator/src/main/java/org/scanerator/WrappingOrderedIterable.java
@@ -6,14 +6,38 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+/**
+ * {@link OrderedIterable} that wraps an {@link Iterable} and maintains
+ * the {@link OrderedIterable} ascending-order contract by checking
+ * returned elements with a {@link Comparator}.  If an out-of-order
+ * element is encountered it is either dropped or an {@link IllegalStateException}
+ * is thrown, depending on the configuration of this {@link WrappingOrderedIterable}.
+ * @author robin
+ *
+ * @param <T>
+ */
 public class WrappingOrderedIterable<T> extends AbstractOrderedIterable<T> {
 
-	protected Iterable<T> itr;
+	/**
+	 * The wrapped {@link Iterable}
+	 */
+	protected Iterable<T> wrapped;
+	/**
+	 * Drop out-of-order elements? {@code true} == drop, {@code false} == exception
+	 */
 	protected boolean dropDescending;
 	
+	/**
+	 * Create an {@link OrderedIterable} wrapping an {@link Iterable}, enforced
+	 * by a {@link Comparator}, that either drops out-of-order elements
+	 * or throws an exception, depending on {@code dropDescending}
+	 * @param itr
+	 * @param cmp
+	 * @param dropDescending
+	 */
 	public WrappingOrderedIterable(Iterable<T> itr, Comparator<? super T> cmp, boolean dropDescending) {
 		super(cmp);
-		this.itr = itr;
+		this.wrapped = itr;
 		this.dropDescending = dropDescending;
 	}
 
@@ -21,9 +45,20 @@ public class WrappingOrderedIterable<T> extends AbstractOrderedIterable<T> {
 		return new Itr();
 	}
 
+	/**
+	 * Iterator that does the wrapping and checking
+	 * @author robin
+	 *
+	 */
 	protected class Itr implements Iterator<T> {
-		protected Iterator<T> itr = WrappingOrderedIterable.this.itr.iterator();
+		/**
+		 * Wrapped {@link Iterator}
+		 */
+		protected Iterator<T> itr = wrapped.iterator();
 		
+		/**
+		 * Upcoming elements
+		 */
 		protected List<T> next = new ArrayList<T>(2);
 		
 		public Itr() {
@@ -41,9 +76,11 @@ public class WrappingOrderedIterable<T> extends AbstractOrderedIterable<T> {
 			if(!hasNext())
 				throw new NoSuchElementException();
 			T n = next.remove(0);
-			if(next.size() > 0 && cmp().compare(n, next.get(0)) > 0) {
-				if(!dropDescending)
+			if(next.size() > 0 && cmp().compare(n, next.get(0)) > 0) { // if the elements are out of order
+				if(!dropDescending) // and if not dropping out-of-order elements
+					// throw an exception
 					throw new IllegalStateException("Not an ascending iterator: " + itr + " found " + n + " followed by " + next.get(0));
+				// otherwise drop elements until an in-order element is found 
 				do {
 					next.remove(0);
 					if(itr.hasNext())
