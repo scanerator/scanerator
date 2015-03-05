9745a9d1e3a535b5c9a621bd01e1e004d655845b
robin <robin.kirkman@gmail.com>
2015-03-04 22:08:17 -0500

    Test for IntersectionOrderedIterable

---
+++ scanerator/src/test/java/org/scanerator/IntersectionOrderedIterableTest.java
@@ -0,0 +1,19 @@
+package org.scanerator;
+
+import static org.scanerator.Scanerator.*;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntersectionOrderedIterableTest {
+	@Test
+	public void testDuplication() {
+		OrderedIterable<Integer> left = itr(Arrays.asList(1, 2, 3, 3, 4, 5));
+		OrderedIterable<Integer> right = itr(Arrays.asList(2, 3, 3, 3, 4));
+		Assert.assertEquals(
+				Arrays.asList(2, 3, 3, 4),
+				list(new IntersectionOrderedIterable<Integer>(left, right)));
+	}
+}
