d630ae674a83fa4a5c1fec35e6287e5fc8599a6b
robin <robin.kirkman@gmail.com>
2015-03-04 20:57:47 -0500

    Javadoc

--- scanerator/src/main/java/org/scanerator/OrderedIterable.java
+++ scanerator/src/main/java/org/scanerator/OrderedIterable.java
@@ -2,6 +2,26 @@ package org.scanerator;
 
 import java.util.Comparator;
 
+/**
+ * {@link Iterable} whose {@link #iterator()} method is
+ * required to return elements in ascending order according
+ * to some {@link Comparator}.<p>
+ * 
+ * Additional useful methods for combining {@link OrderedIterable}
+ * instances are provided.  When two {@link OrderedIterable} instances
+ * are combined with different {@link Comparator}s, the resulting 
+ * {@link OrderedIterable} has for its  {@link Comparator} an
+ * instance of {@link CombinedComparator}, which consults
+ * both {@link Comparator}s for the comparison and throws an
+ * {@link IllegalStateException} if they do not agree.
+ * @author robin
+ *
+ * @param <T>
+ * 
+ * @see Scanerator
+ * @see CombinedComparator
+ * @see AbstractOrderedIterable
+ */
 public interface OrderedIterable<T> extends Iterable<T> {
 	public Comparator<T> cmp();
 	public OrderedIterable<T> any(OrderedIterable<T> i);
