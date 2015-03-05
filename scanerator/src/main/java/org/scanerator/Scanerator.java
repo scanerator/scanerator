df97dbcd5f3a99b4bab272e78f2bcdbdd908596a
robin <robin.kirkman@gmail.com>
2015-03-04 22:58:43 -0500

    Javadoc

--- scanerator/src/main/java/org/scanerator/Scanerator.java
+++ scanerator/src/main/java/org/scanerator/Scanerator.java
@@ -40,26 +40,76 @@ public class Scanerator {
 		return itr(itr, Comparators.<T>naturalOrder(), false);
 	}
 	
+	/**
+	 * Return an {@link OrderedIterable} that has no elements
+	 * @return
+	 */
+	public static <T> OrderedIterable<T> empty() {
+		return new EmptyOrderedIterable<T>();
+	}
+	
+	/**
+	 * Return an {@link OrderedIterable} that is the logical intersection
+	 * of all {@link OrderedIterable}s in the argument {@code itrs}.  If {@code itrs}
+	 * is empty, returns an empty {@link OrderedIterable}.  Otherwise, construct
+	 * a binary tree of {@link IntersectionOrderedIterable} from the
+	 * elements of {@code itrs} and return that.
+	 * @param itrs
+	 * @return
+	 * @see IntersectionOrderedIterable
+	 */
 	public static <T> OrderedIterable<T> all(List<OrderedIterable<T>> itrs) {
-		if(itrs.size() == 0)
+		if(itrs.size() == 0) // edge case, no elements
 			return new EmptyOrderedIterable<T>();
-		if(itrs.size() == 1)
+		if(itrs.size() == 1) // base case, return the only OrderedIterable
 			return itrs.get(0);
+		// divide and conquer
 		List<OrderedIterable<T>> left = itrs.subList(0, itrs.size() / 2);
 		List<OrderedIterable<T>> right = itrs.subList(itrs.size() / 2, itrs.size());
+		// return intersection of all(left) and all(right)
 		return new IntersectionOrderedIterable<T>(all(left), all(right));
 	}
 	
+	/**
+	 * Return an {@link OrderedIterable} that is the logical union
+	 * of all {@link OrderedIterable}s in the argument {@code itrs}.  If
+	 * {@code itrs} is empty, returns an empty {@link OrderedIterable}.
+	 * Otherwise, construct a binary tree of {@link UnionOrderedIterable} from
+	 * the elements of {@code itrs} and return that.
+	 * @param itrs
+	 * @return
+	 * @see UnionOrderedIterable
+	 */
 	public static <T> OrderedIterable<T> any(List<OrderedIterable<T>> itrs) {
-		if(itrs.size() == 0)
+		if(itrs.size() == 0) // edge case, no elements
 			return new EmptyOrderedIterable<T>();
-		if(itrs.size() == 1)
+		if(itrs.size() == 1) // base case, return the only OrderedIterable
 			return itrs.get(0);
+		// divide an conquery
 		List<OrderedIterable<T>> left = itrs.subList(0, itrs.size() / 2);
 		List<OrderedIterable<T>> right = itrs.subList(itrs.size() / 2, itrs.size());
+		// return union of any(left) and any(right)
 		return new UnionOrderedIterable<T>(any(left), any(right));
 	}
 	
+	/**
+	 * Return an {@link OrderedIterable} that is the logical
+	 * subtraction of {@code rhs} from {@code lhs}.
+	 * @param lhs
+	 * @param rhs
+	 * @return
+	 * @see SubtractionOrderedIterable
+	 */
+	public static <T> OrderedIterable<T> not(OrderedIterable<T> lhs, OrderedIterable<T> rhs) {
+		return new SubtractionOrderedIterable<T>(lhs, rhs);
+	}
+	
+	/**
+	 * Build a {@link List} from the contents of an {@link Iterable}
+	 * and return that {@link List}.
+	 * @param itr
+	 * @return
+	 */
 	public static <T> List<T> list(Iterable<T> itr) {
 		List<T> ret = new ArrayList<T>();
 		for(T obj : itr)
--- scanerator/src/main/java/org/scanerator/SubtractionOrderedIterable.java
+++ scanerator/src/main/java/org/scanerator/SubtractionOrderedIterable.java
@@ -3,11 +3,32 @@ package org.scanerator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * {@link OrderedIterable} that is the logical subtraction
+ * of the left-hand side from the right-hand side; this {@link OrderedIterable}
+ * contains all elements of the left-hand side that are not
+ * contained in the right-hand side.
+ * @author robin
+ *
+ * @param <T>
+ */
 public class SubtractionOrderedIterable<T> extends AbstractOrderedIterable<T> {
 
+	/**
+	 * The left-hand side {@link OrderedIterable}
+	 */
 	protected OrderedIterable<T> lhs;
+	/**
+	 * The right-hand side {@link OrderedIterable}
+	 */
 	protected OrderedIterable<T> rhs;
 	
+	/**
+	 * Create an {@link OrderedIterable} that is the logical subtraction
+	 * of {@code lhs} from {@code rhs}.
+	 * @param lhs
+	 * @param rhs
+	 */
 	public SubtractionOrderedIterable(OrderedIterable<T> lhs, OrderedIterable<T> rhs) {
 		super(Comparators.combine(lhs.cmp(), rhs.cmp()));
 		this.lhs = lhs;
@@ -18,23 +39,44 @@ public class SubtractionOrderedIterable<T> extends AbstractOrderedIterable<T> {
 		return new Itr();
 	}
 
+	/**
+	 * {@link Iterator} which does the subtraction
+	 * @author robin
+	 *
+	 */
 	protected class Itr implements Iterator<T> {
+		/**
+		 * LHS {@link Iterator}
+		 */
 		protected Iterator<T> litr = lhs.iterator();
+		/**
+		 * RHS {@link Iterator}
+		 */
 		protected Iterator<T> ritr = rhs.iterator();
 
+		/**
+		 * The next element from the left-hand side
+		 */
 		protected PQ lnext = new PQ(1);
+		/**
+		 * The next element from the right-hand side
+		 */
 		protected PQ rnext = new PQ(1);
 		
+		/**
+		 * Advance {@link #litr} and {@link #ritr} past
+		 * any duplicated elements
+		 */
 		protected void pull() {
-			while(!lnext.isEmpty() && !rnext.isEmpty()) {
+			while(!lnext.isEmpty() && !rnext.isEmpty()) { // only need to advance if both are not empty
 				int c = cmp().compare(lnext.peek(), rnext.peek());
-				if(c < 0)
+				if(c < 0) // if lhs is less than RHS then it is not in RHS
 					return;
-				else if(c == 0) {
+				else if(c == 0) { // if lhs == rhs then drop it and try again
 					lnext.poll();
 					if(litr.hasNext())
 						lnext.offer(litr.next());
-				} else {
+				} else { // if lhs is greater than RHS then advance RHS
 					rnext.poll();
 					if(ritr.hasNext())
 						rnext.offer(ritr.next());
